7323dd5e5a45092566579b79436e316d8a24cbb5
octofi <hello@octo.fi>
2020-10-03 03:14:51 +0000

    Updates

---
+++ precache-manifest.2880f00d555063aa46fa851c46c0769e.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d86a919c578c486aa17a4ec4daacee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "171c4934b85acd126fd2",
+    "url": "/static/css/2.33a082aa.chunk.css"
+  },
+  {
+    "revision": "287151f0a882730dcf2e",
+    "url": "/static/css/main.ce2a5526.chunk.css"
+  },
+  {
+    "revision": "171c4934b85acd126fd2",
+    "url": "/static/js/2.81d934d6.chunk.js"
+  },
+  {
+    "revision": "aea0cbe053a20c3078b4fdc29c7dbb8a",
+    "url": "/static/js/2.81d934d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccf781b3c89c6216fb0",
+    "url": "/static/js/3.eb3239f7.chunk.js"
+  },
+  {
+    "revision": "95b85fc57f7f49a2f3a98124773ccfef",
+    "url": "/static/js/3.eb3239f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b267fe90c16e4cb1dc",
+    "url": "/static/js/4.f39b0330.chunk.js"
+  },
+  {
+    "revision": "2159f6dd280b81d7d7b1",
+    "url": "/static/js/5.69d46e9c.chunk.js"
+  },
+  {
+    "revision": "287151f0a882730dcf2e",
+    "url": "/static/js/main.eeea0fa0.chunk.js"
+  },
+  {
+    "revision": "029c6601ed090a6b444d09258de6b8ae",
+    "url": "/static/js/main.eeea0fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00fa2d1b8fdfa13b983b",
+    "url": "/static/js/runtime-main.2c93fa18.js"
+  },
+  {
+    "revision": "b35a142d7a9bbaef9c797cc7f4715d2d",
+    "url": "/static/media/AUD.b35a142d.svg"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/static/media/BTC.d7876d48.svg"
+  },
+  {
+    "revision": "5d08ccab9059d0350d3777b7a708c3b8",
+    "url": "/static/media/CAD.5d08ccab.svg"
+  },
+  {
+    "revision": "c561bca49e9c856205a6c730c91a6dbf",
+    "url": "/static/media/CHF.c561bca4.svg"
+  },
+  {
+    "revision": "87d6c3511f48ad35dd20f39111fcf86a",
+    "url": "/static/media/CNY.87d6c351.svg"
+  },
+  {
+    "revision": "6d1ffc6c10d40230c513fa7f8c9b7622",
+    "url": "/static/media/ETH.6d1ffc6c.svg"
+  },
+  {
+    "revision": "c248841998c12fbd99cfce5d67baa9cf",
+    "url": "/static/media/EUR.c2488419.svg"
+  },
+  {
+    "revision": "35d544eaaa4cf3c6355866280d53ba73",
+    "url": "/static/media/Flaticon.35d544ea.eot"
+  },
+  {
+    "revision": "3e4331ee31764c999add7e0b048c4ba3",
+    "url": "/static/media/Flaticon.3e4331ee.ttf"
+  },
+  {
+    "revision": "5be3e43c13c3eb021d15e6682d098d4c",
+    "url": "/static/media/Flaticon.5be3e43c.woff"
+  },
+  {
+    "revision": "c1729513a8741b7b61bae040816e426f",
+    "url": "/static/media/Flaticon.c1729513.svg"
+  },
+  {
+    "revision": "29586ff0f963f4d1fdfc182822b8b27a",
+    "url": "/static/media/Flaticon2.29586ff0.eot"
+  },
+  {
+    "revision": "b242ac810bd8cccaa03abc2128b7c3c3",
+    "url": "/static/media/Flaticon2.b242ac81.woff"
+  },
+  {
+    "revision": "e1e2b6e05bbfd279181c693555c61bad",
+    "url": "/static/media/Flaticon2.e1e2b6e0.svg"
+  },
+  {
+    "revision": "eafcbac04cdb0a39fe38a36ebd786290",
+    "url": "/static/media/Flaticon2.eafcbac0.ttf"
+  },
+  {
+    "revision": "db68882cbf4beac3f3ee3923f1a91b82",
+    "url": "/static/media/GBP.db68882c.svg"
+  },
+  {
+    "revision": "b5c00b410e5a59bc5c433ebcf9c19550",
+    "url": "/static/media/INR.b5c00b41.svg"
+  },
+  {
+    "revision": "f8dc0f044c962d058420eb35d2228f83",
+    "url": "/static/media/JPY.f8dc0f04.svg"
+  },
+  {
+    "revision": "793c8ae22dd469b9274e4002768beabb",
+    "url": "/static/media/KRW.793c8ae2.svg"
+  },
+  {
+    "revision": "2a5ff834d12f21b421748941d6057d04",
+    "url": "/static/media/Ki.2a5ff834.ttf"
+  },
+  {
+    "revision": "31f3692ca10d2f6d0bc467adba4533f7",
+    "url": "/static/media/Ki.31f3692c.eot"
+  },
+  {
+    "revision": "451fd5bee09962b6977544325ace633f",
+    "url": "/static/media/Ki.451fd5be.svg"
+  },
+  {
+    "revision": "8f9a1a6cffbd1826644e12714ec106d0",
+    "url": "/static/media/Ki.8f9a1a6c.woff"
+  },
+  {
+    "revision": "3f226a426906fafba58c9d8ff8cdd11d",
+    "url": "/static/media/LINK.3f226a42.svg"
+  },
+  {
+    "revision": "4f6065d8ae0660a7e87b3ec0a840df3e",
+    "url": "/static/media/NZD.4f6065d8.svg"
+  },
+  {
+    "revision": "562474efbaa31ee8de76df31c4bd2116",
+    "url": "/static/media/RUB.562474ef.svg"
+  },
+  {
+    "revision": "deda081753f0861b1d8b17ea8e939df5",
+    "url": "/static/media/SGD.deda0817.svg"
+  },
+  {
+    "revision": "936feee6e8061e6bf4006dce278384dd",
+    "url": "/static/media/USD.936feee6.svg"
+  },
+  {
+    "revision": "904b44c2b22eb2d49f618396e6f2db1a",
+    "url": "/static/media/blue-loader.904b44c2.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "/static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "844ac0d0fe59dc4d60a977e32456694f",
+    "url": "/static/media/ledger.844ac0d0.svg"
+  },
+  {
+    "revision": "c879a582a94e772b8ed27295ae83145d",
+    "url": "/static/media/metamask.c879a582.svg"
+  },
+  {
+    "revision": "60e5857089e98edd838074c264d6c951",
+    "url": "/static/media/socicon.60e58570.eot"
+  },
+  {
+    "revision": "944f06f5f65ef84a3a36e6c1c2d4b7ad",
+    "url": "/static/media/socicon.944f06f5.woff"
+  },
+  {
+    "revision": "9a64ef938f9e55a70a4defc6ac9790bf",
+    "url": "/static/media/socicon.9a64ef93.ttf"
+  },
+  {
+    "revision": "a35b65744f557fab5424e99bb6d4e980",
+    "url": "/static/media/socicon.a35b6574.svg"
+  },
+  {
+    "revision": "e12a83718eed35b2f29ff79a2c211310",
+    "url": "/static/media/trezor.e12a8371.svg"
+  },
+  {
+    "revision": "82b1c467a507265ecaafe1004abf5049",
+    "url": "/static/media/trustwallet-i.82b1c467.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/walletconnect.37f2bc6e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/octofi-app-aquafarm/precache-manifest.0911b4d7a1ff601c75e3a8e4fac3df02.js"
+  "/precache-manifest.2880f00d555063aa46fa851c46c0769e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/octofi-app-aquafarm/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
