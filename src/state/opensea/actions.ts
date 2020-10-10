0ee4d6a5e342d12343f3554e4adb323374fc87d6
nathanjessen <nathan.jessen@gmail.com>
2020-10-10 10:51:14 -0600

    Create opensea actions

---
+++ src/state/opensea/actions.ts
@@ -0,0 +1,34 @@
+import { createAction } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { openseaAssets } from "../../http/opensea";
+
+export const setOpenSeaAssets = createAction('opensea/setAssets', (assets) => {
+    return {
+        payload: {
+            assets
+        }
+    }
+});
+
+export const fetchOpenSeaAssets = (owner: string) => {
+    return async (dispatch: (arg0: { payload: { assets: Array<Object>; } }) => void) => {
+        try {
+            const params = {
+                owner,
+                asset_contract_addresses: '[]',
+                order_direction: 'desc',
+                offset: '0',
+                limit: '20'
+            };
+            const res = await axios.get(openseaAssets, { params });
+
+            const assets = {
+                ...res.data.assets
+            };
+
+            dispatch(setOpenSeaAssets(assets));
+        } catch(error) {
+            console.log(error);
+        }
+    }
+};
