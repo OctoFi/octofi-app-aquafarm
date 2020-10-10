d4dba7023998c817830536d52c570d02f0ba1af7
nathanjessen <nathan.jessen@gmail.com>
2020-10-10 16:50:29 -0600

    Create opensea reducer

---
+++ src/state/opensea/reducer.ts
@@ -0,0 +1,15 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { setOpenSeaAssets } from './actions';
+
+const initialState = {
+  assets: []
+};
+
+export default createReducer(initialState, builder =>
+  builder.addCase(setOpenSeaAssets, (state, { payload: { assets } }) => {
+        return {
+            ...state,
+            ...assets
+        }
+    })
+);
