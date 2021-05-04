5e9ec0f9ffb650577ed5ecf5f458adae60da9866
octofi <hello@octo.fi>
2021-05-04 15:56:12 +0000

    Updates

---
+++ static/js/49.97cd7be1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdefi_v2=this.webpackJsonpdefi_v2||[]).push([[49],{3781:function(i,n){},3877:function(i,n){}}]);
+//# sourceMappingURL=49.97cd7be1.chunk.js.map
--- static/js/50.d7e43466.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdefi_v2=this.webpackJsonpdefi_v2||[]).push([[50],{3789:function(i,n){},3885:function(i,n){}}]);
-//# sourceMappingURL=50.d7e43466.chunk.js.map
