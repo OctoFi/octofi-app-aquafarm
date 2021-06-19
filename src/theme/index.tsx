3991e9d8af4a3575299703fed6a86e32324b3194
nathanjessen <nathan.jessen@gmail.com>
2021-06-19 14:21:47 -0500

    Update bg5

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -50,7 +50,7 @@ export function colors(darkMode?: boolean): Colors {
 		bg2: darkMode ? "#232429" : white,
 		bg3: darkMode ? "rgba(33, 36, 41, 1)" : "#FFF",
 		bg4: darkMode ? "#232429" : "rgb(212, 218, 242)",
-		bg5: darkMode ? `rgba(255, 255, 255, 0.15) ` : `rgba(0, 0, 0, 0.15)`,
+		bg5: darkMode ? `rgba(255, 255, 255, 0.1) ` : `rgba(0, 0, 0, 0.05)`,
 
 		// UI
 		borderColor: darkMode ? `rgba(255, 255, 255, 0.15) ` : `rgba(0, 0, 0, 0.15)`,
