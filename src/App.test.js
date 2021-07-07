c13ea3ded9b46106389a3cfeb610ec092bcfa1f6
Delano Cooper <delanoscooper@gmail.com>
2021-07-07 17:24:35 -0400

    add initial smoke tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "./testing/customRender";
 import App from "./App";
 
-test("renders learn react link", () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+jest.mock("./state/lists/updater", () => jest.fn());
+jest.mock("./theme", () => ({
+	FixedGlobalStyle: () => "FixedGlobalStyle",
+	ThemedGlobalStyle: () => "ThemedGlobalStyle",
+}));
+
+jest.mock("./components/RouteChanger/routeChanger", () => () => <div>RouteChanger</div>);
+jest.mock("./components/WalletModal", () => () => <div>WalletModal</div>);
+jest.mock("./state/lists/updater", () => () => <div>ListsUpdater</div>);
+jest.mock("./state/application/updater", () => () => <div>ApplicationUpdater</div>);
+jest.mock("./state/multicall/updater", () => () => <div>MultiCallUpdater</div>);
+jest.mock("./state/transactions/updater", () => () => <div>TransactionUpdater</div>);
+jest.mock("./state/user/updater", () => () => <div>UserUpdater</div>);
+jest.mock("./Routes", () => () => <div>Routes</div>);
+jest.mock("./components/Web3ReactManager", () => ({ children }) => <div>Web3ReactManager {children}</div>);
+jest.mock("./components/TransactionHandler", () => () => <div>TransactionHandler</div>);
+
+let state = {
+	initialState: {
+		user: {
+			userDarkMode: false,
+		},
+	},
+};
+
+test("smoke test light mode", () => {
+	render(<App />, state);
+});
+
+test("smoke test dark mode", () => {
+	state.initialState.user.userDarkMode = true;
+	render(<App />, state);
+});
+
+test("requires user state", () => {
+	state.initialState.user = null;
+	expect(() => render(<App />, state)).toThrow();
 });
