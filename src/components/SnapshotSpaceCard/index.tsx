d24a17dcc8d8f49a4bb1e0dd4ef24cdf3b8ebc33
nathanjessen <nathan.jessen@gmail.com>
2021-07-10 20:28:34 -0500

    Separate loading state

--- src/components/SnapshotSpaceCard/index.tsx
+++ src/components/SnapshotSpaceCard/index.tsx
@@ -16,27 +16,38 @@ export type SnapshotSpaceCardProps = {
 
 const SnapshotSpaceCard = ({ space, id, pinned = false, symbolIndex, loading = false }: SnapshotSpaceCardProps) => {
 	const LogoURL = useLogo(id, symbolIndex);
-	return (
-		<Styled.Wrapper to={loading ? "#" : `/governance/${id}`} loading={loading}>
-			{!loading && pinned && (
-				<Styled.StarWrapper>
-					<SVG src={PinStartIcon} width={16} height={16} />
-				</Styled.StarWrapper>
-			)}
-			{loading ? (
+
+	if (loading) {
+		return (
+			<Styled.Wrapper to={"#"} loading={loading}>
 				<Skeleton
 					circle={true}
 					width={isMobile ? 100 : 66}
 					height={isMobile ? 100 : 66}
 					style={{ marginBottom: "1.25rem" }}
 				/>
-			) : (
-				<Styled.Logo src={LogoURL} />
+
+				<Styled.Title>
+					<Skeleton height={20} width={isMobile ? 80 : 120} />
+				</Styled.Title>
+				<Styled.CurrencyName>
+					<Skeleton height={15} width={isMobile ? 50 : 80} />
+				</Styled.CurrencyName>
+			</Styled.Wrapper>
+		);
+	}
+
+	return (
+		<Styled.Wrapper to={`/governance/${id}`} loading={loading}>
+			{pinned && (
+				<Styled.StarWrapper>
+					<SVG src={PinStartIcon} width={16} height={16} />
+				</Styled.StarWrapper>
 			)}
-			<Styled.Title>{loading ? <Skeleton height={20} width={isMobile ? 80 : 120} /> : space.name}</Styled.Title>
-			<Styled.CurrencyName>
-				{loading ? <Skeleton height={15} width={isMobile ? 50 : 80} /> : space.symbol}
-			</Styled.CurrencyName>
+
+			<Styled.Logo src={LogoURL} />
+			<Styled.Title>{space.name}</Styled.Title>
+			<Styled.CurrencyName>{space.symbol}</Styled.CurrencyName>
 		</Styled.Wrapper>
 	);
 };
