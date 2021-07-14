5a127dde1c0fa93b1fb299eca82e5b2f58989635
nathanjessen <nathan.jessen@gmail.com>
2021-07-14 16:06:00 -0500

    Add default CurrencyLogo size

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -10,14 +10,14 @@ import Logo from "../TokenLogo";
 const getTokenLogoURL = (address: string) =>
 	`https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`;
 
-const StyledEthereumLogo = styled.img<{ size?: number | string }>`
+const StyledEthereumLogo = styled.img<{ size: number | string }>`
 	border-radius: ${({ size }) => `${size}px` || "100%"};
 	color: ${({ theme }) => theme.text1};
 	width: ${({ size }) => `${size}px` || "100%"};
 	height: ${({ size }) => `${size}px` || "100%"};
 `;
 
-const StyledTokenLogo = styled(Logo)<{ size?: number | string }>`
+const StyledTokenLogo = styled(Logo)<{ size: number | string }>`
 	border-radius: ${({ size }) => `${size}px` || "100%"};
 	color: ${({ theme }) => theme.text1};
 	width: ${({ size }) => `${size}px` || "100%"};
@@ -26,11 +26,11 @@ const StyledTokenLogo = styled(Logo)<{ size?: number | string }>`
 
 export type CurrencyLogoProps = {
 	currency?: Currency;
-	size?: string;
+	size?: number | string;
 	style?: React.CSSProperties;
 };
 
-export default function CurrencyLogo({ currency, size, style }: CurrencyLogoProps) {
+export default function CurrencyLogo({ currency, size = 32, style }: CurrencyLogoProps) {
 	const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined);
 
 	const srcs: string[] = useMemo(() => {
